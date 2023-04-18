bfc4b5244aa50cb243eaea8df82bdf719ee2ab97
kimbela winston <kimbelawin84@gmail.com>
2023-04-18 06:17:08 -0500

    results

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+      const win = record.find(obj=> obj.result === 'W');
+      if (win) {
+        return win.year;
+      } else {
+        return undefined;
+      }
+}
